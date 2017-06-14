84ac462b32ffc0110a2460de17883139b85c5843
Mahi <manadirmahi@gmail.com>
2017-06-14 16:22:49 +0600

    facebook login using jwt

---
+++ FBLogin/Client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+import routes from './routes';
+import reducers from './reducers/index';
+import { AUTH_USER } from './actions/types';
+import './public/stylesheets/base.scss';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.querySelector('.wrapper'));
+  import cookie from 'react-cookie';
+  const token = cookie.load('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
